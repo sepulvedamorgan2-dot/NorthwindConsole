1285fc1452b510b1c180dbb5ed630b3abd5e62a9
mepul <sepulvedamorgan2@gmail.com>
2026-04-16 13:43:58 -0500

    1.11 + 1.12

--- Model/Category.cs
+++ Model/Category.cs
@@ -1,15 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace NorthwindConsole.Model;
 
 public partial class Category
 {
-    public int CategoryId { get; set; }
+  public int CategoryId { get; set; }
+  [Required]
+  public string CategoryName { get; set; } = null!;
 
-    public string CategoryName { get; set; } = null!;
+  public string? Description { get; set; }
 
-    public string? Description { get; set; }
-
-    public virtual ICollection<Product> Products { get; set; } = new List<Product>();
+  public virtual ICollection<Product> Products { get; set; } = new List<Product>();
 }
--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using Microsoft.Extensions.Configuration;
 using Microsoft.EntityFrameworkCore;
 using NorthwindConsole.Model;
+using System.ComponentModel.DataAnnotations;
 string path = Directory.GetCurrentDirectory() + "//nlog.config";
 
 // create instance of Logger
@@ -12,42 +13,63 @@ logger.Info("Program started");
 
 do
 {
-    Console.WriteLine("1) Display categories");
-    Console.WriteLine("2) Add category");
-    Console.WriteLine("Enter to quit");
-    string? choice = Console.ReadLine();
-    Console.Clear();
-    logger.Info("Option {choice} selected", choice);
+  Console.WriteLine("1) Display categories");
+  Console.WriteLine("2) Add category");
+  Console.WriteLine("Enter to quit");
+  string? choice = Console.ReadLine();
+  Console.Clear();
+  logger.Info("Option {choice} selected", choice);
 
-    if (choice == "1")
+  if (choice == "1")
+  {
+    // display categories
+    var configuration = new ConfigurationBuilder()
+            .AddJsonFile($"appsettings.json");
+
+    var config = configuration.Build();
+
+    var db = new DataContext();
+    var query = db.Categories.OrderBy(p => p.CategoryName);
+
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"{query.Count()} records returned");
+    Console.ForegroundColor = ConsoleColor.Magenta;
+    foreach (var item in query)
     {
-        
-        var configuration = new ConfigurationBuilder()
-               .AddJsonFile($"appsettings.json");
-
-        var config = configuration.Build();
-
-        var db = new DataContext();
-        var query = db.Categories.OrderBy(p => p.CategoryName);
-
-        Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine($"{query.Count()} records returned");
-        Console.ForegroundColor = ConsoleColor.Magenta;
-        foreach (var item in query)
-        {
-            Console.WriteLine($"{item.CategoryName} - {item.Description}");
-        }
-        Console.ForegroundColor = ConsoleColor.White;
+      Console.WriteLine($"{item.CategoryName} - {item.Description}");
     }
-    else if (choice == "2")
+    Console.ForegroundColor = ConsoleColor.White;
+  }
+  else if (choice == "2")
+  {
+    // Add category
+    Category category = new();
+    Console.WriteLine("Enter Category Name:");
+    category.CategoryName = Console.ReadLine()!;
+    Console.WriteLine("Enter the Category Description:");
+    category.Description = Console.ReadLine();
+    ValidationContext context = new ValidationContext(category, null, null);
+    List<ValidationResult> results = new List<ValidationResult>();
+
+    var isValid = Validator.TryValidateObject(category, context, results, true);
+    if (isValid)
     {
-        // Add category
+      logger.Info("Validation passed");
+      // TODO: save category to db
     }
-    else if (String.IsNullOrEmpty(choice))
+    if (!isValid)
     {
-        break;
+      foreach (var result in results)
+      {
+        logger.Error($"{result.MemberNames.First()} : {result.ErrorMessage}");
+      }
     }
-    Console.WriteLine();
+  }
+  else if (String.IsNullOrEmpty(choice))
+  {
+    break;
+  }
+  Console.WriteLine();
 } while (true);
 
 logger.Info("Program ended");
