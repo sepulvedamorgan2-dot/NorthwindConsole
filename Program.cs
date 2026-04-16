9efe4f10a81f6386884bd13a6c7ebb497a1463d4
mepul <sepulvedamorgan2@gmail.com>
2026-04-16 13:27:15 -0500

    1.10 Display Categories

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using NLog;
 using System.Linq;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
 using NorthwindConsole.Model;
 string path = Directory.GetCurrentDirectory() + "//nlog.config";
 
@@ -10,26 +12,42 @@ logger.Info("Program started");
 
 do
 {
-  Console.WriteLine("1) Display categories");
-  Console.WriteLine("2) Add category");
-  Console.WriteLine("Enter to quit");
-  string? choice = Console.ReadLine();
-  Console.Clear();
-  logger.Info("Option {choice} selected", choice);
+    Console.WriteLine("1) Display categories");
+    Console.WriteLine("2) Add category");
+    Console.WriteLine("Enter to quit");
+    string? choice = Console.ReadLine();
+    Console.Clear();
+    logger.Info("Option {choice} selected", choice);
 
-  if (choice == "1")
-  {
-    // display categories
-  }
-  else if (choice == "2")
-  {
-    // Add category
-  }
-  else if (String.IsNullOrEmpty(choice))
-  {
-    break;
-  }
-  Console.WriteLine();
+    if (choice == "1")
+    {
+        
+        var configuration = new ConfigurationBuilder()
+               .AddJsonFile($"appsettings.json");
+
+        var config = configuration.Build();
+
+        var db = new DataContext();
+        var query = db.Categories.OrderBy(p => p.CategoryName);
+
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine($"{query.Count()} records returned");
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        foreach (var item in query)
+        {
+            Console.WriteLine($"{item.CategoryName} - {item.Description}");
+        }
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+    else if (choice == "2")
+    {
+        // Add category
+    }
+    else if (String.IsNullOrEmpty(choice))
+    {
+        break;
+    }
+    Console.WriteLine();
 } while (true);
 
 logger.Info("Program ended");
