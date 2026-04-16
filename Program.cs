97b4deaf673c7cc8fd91bc3a2b7741fd9a802b9f
mepul <sepulvedamorgan2@gmail.com>
2026-04-16 13:24:39 -0500

    1.09

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿﻿using NLog;
+﻿using NLog;
+using System.Linq;
+using NorthwindConsole.Model;
 string path = Directory.GetCurrentDirectory() + "//nlog.config";
 
 // create instance of Logger
@@ -6,6 +8,28 @@ var logger = LogManager.Setup().LoadConfigurationFromFile(path).GetCurrentClassL
 
 logger.Info("Program started");
 
-Console.WriteLine("Hello World!");
+do
+{
+  Console.WriteLine("1) Display categories");
+  Console.WriteLine("2) Add category");
+  Console.WriteLine("Enter to quit");
+  string? choice = Console.ReadLine();
+  Console.Clear();
+  logger.Info("Option {choice} selected", choice);
+
+  if (choice == "1")
+  {
+    // display categories
+  }
+  else if (choice == "2")
+  {
+    // Add category
+  }
+  else if (String.IsNullOrEmpty(choice))
+  {
+    break;
+  }
+  Console.WriteLine();
+} while (true);
 
 logger.Info("Program ended");
